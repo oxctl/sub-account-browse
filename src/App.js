0c957b0cbde8607e5dba29119d874aa4ee91355d
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-04-30 10:35:03 +0100

    Update to use the production S3 buckets
    We have new S3 buckets having moved to production.

--- src/App.js
+++ src/App.js
@@ -42,6 +42,14 @@ const settings = {
   'https://oxctl-subaccounts.s3-eu-west-1.amazonaws.com': {
     'ltiServer': 'https://lti.canvas.ox.ac.uk',
     'proxyServer': 'https://proxy.canvas.ox.ac.uk'
+  },
+  'https://oxctl-canvas-subaccounts-dev.s3-eu-west-1.amazonaws.com': {
+    'ltiServer': 'https://lti.canvas.ox.ac.uk',
+    'proxyServer': 'https://proxy.canvas.ox.ac.uk'
+  },
+  'https://oxctl-canvas-subaccounts-prod.s3-eu-west-1.amazonaws.com': {
+    'ltiServer': 'https://lti.canvas.ox.ac.uk',
+    'proxyServer': 'https://proxy.canvas.ox.ac.uk'
   }
 }
 
