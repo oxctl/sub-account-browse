4aee58176b6d88de2c017088eb504c94999a0fc5
christopherjohnson <christophersebastianjohnson@gmail.com>
2023-04-19 14:31:16 +0100

    fix claims error

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends React.Component {
     const jwt = jwtDecode(token)
     this.setState({
       comInstructureBrandConfigJsonUrl: jwt['https://purl.imsglobal.org/spec/lti/claim/custom'].com_instructure_brand_config_json_url,
-      canvasUserPrefersHighContrast: this.jwt['https://purl.imsglobal.org/spec/lti/claim/custom'].canvas_user_prefers_high_contrast === "true",
+      canvasUserPrefersHighContrast: jwt['https://purl.imsglobal.org/spec/lti/claim/custom'].canvas_user_prefers_high_contrast === "true",
       accountId: parseInt(jwt['https://purl.imsglobal.org/spec/lti/claim/custom'].canvas_account_id),
       accountName: jwt['https://purl.imsglobal.org/spec/lti/claim/custom'].canvas_account_name,
       canvasUrl: jwt['https://purl.imsglobal.org/spec/lti/claim/custom'].canvas_api_base_url,
