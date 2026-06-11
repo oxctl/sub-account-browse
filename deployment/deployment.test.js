5135f100e9cecef6cef90f392f4f3f995aa80b92
christopherjohnson <christophersebastianjohnson@gmail.com>
2026-06-11 11:38:56 +0100

    AB#126596 upgrade deployment-test-utils  * add skipped test warning

--- deployment/deployment.test.js
+++ deployment/deployment.test.js
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test'
 import { dismissBetaBanner, getLtiIFrame, waitForNoSpinners, TEST_URL, grantAccessIfNeeded } from '@oxctl/deployment-test-utils'
 
 test.describe('Test deployment', () => {
-  test.skip(true, 'Blocked by upstream issue: ADO-125227 https://oxforduniversity.visualstudio.com/Canvas/_workitems/edit/125227')
-  test('The tool should load and display, (amongst other things,) a Find button.', async ({ page, context }) => {
+  // Skipped due to upstream issue: ADO-125227 https://oxforduniversity.visualstudio.com/Canvas/_workitems/edit/125227
+  test.fixme('The tool should load and display, (amongst other things,) a Find button.', async ({ page, context }) => {
     await page.goto(TEST_URL)
     await dismissBetaBanner(page)
     await grantAccessIfNeeded(page, context, TEST_URL)
